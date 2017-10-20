da6024a63a2c07129910c647fa35b389623c2df7
adrielsales <adrielsalles@gmail.com>
2017-10-20 14:09:16 -0300

    criando repositório e adicionando primeiros arquivos

---
+++ includes/header.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<title>Loja Programador Web - Dashboard</title>
+	<base href="http://localhost/senac/programadorWeb2017/sources/php/loja/">
+	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
+	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
+
+	<!-- include summernote css/js-->
+	<link href="recursos/summernote/summernote.css" rel="stylesheet">
+
+	<link rel="stylesheet" href="recursos/css/loja.css">
+</head>
+<body>
